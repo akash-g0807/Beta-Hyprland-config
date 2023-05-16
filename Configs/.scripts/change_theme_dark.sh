1c276c48889c2b788c82e7c98dc78bc05ff1e244
Akash <ag1884@york.ac.uk>
2023-05-16 14:54:18 +0100

    Updated theme scripts and removed redundant scripts

--- Configs/.scripts/change_theme_dark.sh
+++ Configs/.scripts/change_theme_dark.sh
@@ -1,3 +1,9 @@
 #!/bin/sh
 
 sed -ri 's/gtk-theme-name=.*/gtk-theme-name=Catppuccin-Mocha-Standard-Blue-Dark/g' ~/.config/gtk-3.0/settings.ini
+
+sed -ri 's/gtk-icon-theme-name=.*/gtk-icon-theme-name=Adwaita/g' ~/.config/gtk-3.0/settings.ini
+
+sed -ri 's/gtk-cursor-theme-name=.*/gtk-cursor-theme-name=Nordzy-cursors/g' ~/.config/gtk-3.0/settings.ini
+
+
--- Configs/.scripts/change_theme_light.sh
+++ Configs/.scripts/change_theme_light.sh
@@ -1,3 +1,7 @@
 #!/bin/sh
 
 sed -ri 's/gtk-theme-name=.*/gtk-theme-name=Catpuccin-Latte-Standard-Blue-Light/g' ~/.config/gtk-3.0/settings.ini
+
+sed -ri 's/gtk-icon-theme-name=.*/gtk-icon-theme-name=Adwaita/g' ~/.config/gtk-3.0/settings.ini
+
+sed -ri 's/gtk-cursor-theme-name=.*/gtk-cursor-theme-name=Nordzy-cursors-white/g' ~/.config/gtk-3.0/settings.ini
