6296f364bcb8bfd9fdf52833718dce05a0a803b1
Akash <ag1884@york.ac.uk>
2023-05-16 16:56:32 +0100

    Fixed theming change not working

--- Configs/.config/hypr/hypr/sleep-monitor.sh
+++
@@ -1,7 +0,0 @@
-if grep open /proc/acpi/button/lid/LID*/state; then
-    hyprctl keyword monitor "eDP-1, 1920x1080, 0x0, 1"
-else
-    if [[ `hyprctl monitors | grep "Monitor" | wc -l` != 1 ]]; then
-        hyprctl keyword monitor "eDP-1, disable"
-    fi
-fi
