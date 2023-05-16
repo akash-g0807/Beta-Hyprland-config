9daeadeb5aa49d4b066f7f017333b402da0691fd
Akash <ag1884@york.ac.uk>
2023-05-16 17:54:44 +0100

    Updated change_theme*.sh script

--- Configs/.scripts/change_theme_dark.sh
+++ Configs/.scripts/change_theme_dark.sh
@@ -6,7 +6,12 @@ sed -ri 's/gtk-icon-theme-name=.*/gtk-icon-theme-name=Adwaita/g' ~/.config/gtk-3
 
 sed -ri 's/gtk-cursor-theme-name=.*/gtk-cursor-theme-name=Nordzy-cursors/g' ~/.config/gtk-3.0/settings.ini
 
+sed -ri 's|Net/ThemeName.*|Net/ThemeName "Catppuccin-Mocha-Standard-Blue-Dark"|g' ~/.config/xsettingsd/xsettingsd.conf
+sed -ri 's|Net/IconThemeName.*|Net/IconThemeName "ePapirus-Dark"|g' ~/.config/xsettingsd/xsettingsd.conf
+sed -ri 's|Gtk/CursorThemeName.*|Gtk/CursorThemeName "Nordzy-cursors"|g' ~/.config/xsettingsd/xsettingsd.conf
+
 flatpak --user override --env=GTK_THEME=Catppuccin-Mocha-Standard-Blue-Dark
 flatpak --user override --env=ICON_THEME=Papirus-Dark
 flatpak --user override --env=XCURSOR_THEME=Nordzy-cursors
+
 
--- Configs/.scripts/change_theme_light.sh
+++ Configs/.scripts/change_theme_light.sh
@@ -6,6 +6,10 @@ sed -ri 's/gtk-icon-theme-name=.*/gtk-icon-theme-name=Adwaita/g' ~/.config/gtk-3
 
 sed -ri 's/gtk-cursor-theme-name=.*/gtk-cursor-theme-name=Nordzy-cursors-white/g' ~/.config/gtk-3.0/settings.ini
 
+sed -ri 's|Net/ThemeName.*|Net/ThemeName "Catppuccin-Latte-Standard-Blue-Light"|g' ~/.config/xsettingsd/xsettingsd.conf
+sed -ri 's|Net/IconThemeName.*|Net/IconThemeName "ePapirus-Light"|g' ~/.config/xsettingsd/xsettingsd.conf
+sed -ri 's|Gtk/CursorThemeName.*|Gtk/CursorThemeName "Nordzy-cursors-white"|g' ~/.config/xsettingsd/xsettingsd.conf
+
 flatpak --user override --env=GTK_THEME=Catppuccin-Latte-Standard-Blue-Light
 flatpak --user override --env=ICON_THEME=Papirus-Light
 flatpak --user override --env=XCURSOR_THEME=Nordzy-cursors-white
