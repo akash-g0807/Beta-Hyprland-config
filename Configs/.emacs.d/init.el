b37947afe98d7cbff031556fd3b3236ed00526b9
Akash <ag1884@york.ac.uk>
2023-05-24 15:53:17 +0100

    Updated init.el

--- Configs/.emacs.d/init.el
+++ Configs/.emacs.d/init.el
@@ -597,6 +597,20 @@
 (use-package visual-fill-column
   :hook (org-mode . efs/org-mode-visual-fill))
 
+;; ORG FACES
+
+(use-package org-faces
+  :ensure nil
+  :custom
+  (org-todo-keyword-faces
+   '(("DONE"    . (:foreground "#8abeb7" :weight bold))
+     ("NEXT"    . (:foreground "#f0c674" :weight bold))
+     ("SOMEDAY" . (:foreground "#b294bb" :weight bold))
+     ("TODO"    . (:foreground "#b5bd68" :weight bold))
+     ("WAITING" . (:foreground "#de935f" :weight bold)))))
+
+
+
 ;;;;;;;;;;;;;;;;;;;;;; ORG  BABEL ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 (with-eval-after-load 'org
   (org-babel-do-load-languages
@@ -625,9 +639,372 @@
 
 (add-hook 'org-mode-hook (lambda () (add-hook 'after-save-hook #'efs/org-babel-tangle-config)))
 
-
 ;;;;;;;;;;;;;;;;;;;;;; ORG  BABEL ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 
+
+(use-package org
+  :delight "Θ"
+  :hook (org-mode . turn-off-auto-fill)
+  :bind ("C-c i" . org-insert-structure-template)
+  :preface
+  (defun my/org-archive-done-tasks ()
+    "Archive finished or cancelled tasks."
+    (interactive)
+    (org-map-entries
+     (lambda ()
+       (org-archive-subtree)
+       (setq org-map-continue-from (outline-previous-heading)))
+     "TODO=\"DONE\"|TODO=\"CANCELLED\"" (if (org-before-first-heading-p) 'file 'tree)))
+
+  (defun my/org-jump ()
+    "Jump to a specific task."
+    (interactive)
+    (let ((current-prefix-arg '(4)))
+      (call-interactively 'org-refile)))
+
+  (defun my/org-use-speed-commands-for-headings-and-lists ()
+    "Activate speed commands on list items too."
+    (or (and (looking-at org-outline-regexp) (looking-back "^\**"))
+        (save-excursion (and (looking-at (org-item-re)) (looking-back "^[ \t]*")))))
+
+  (defmacro ignore-args (fnc)
+    "Returns function that ignores its arguments and invokes FNC."
+    `(lambda (&rest _rest)
+       (funcall ,fnc)))
+  :hook ((after-save . my/config-tangle)
+         (auto-save . org-save-all-org-buffers)
+         (org-mode . visual-line-mode))
+  :custom
+  (org-archive-location "~/.personal/archives/%s::")
+  (org-blank-before-new-entry '((heading . t)
+                                (plain-list-item . t)))
+  (org-confirm-babel-evaluate nil)
+  (org-cycle-include-plain-lists 'integrate)
+  (org-ellipsis " ▾")
+  (org-export-backends '(ascii beamer html icalendar latex man md org texinfo))
+  (org-hide-emphasis-markers t)
+  (org-log-done 'time)
+  (org-log-into-drawer t)
+  (org-modules '(org-crypt
+                 org-habit
+                 org-mouse
+                 org-protocol
+                 org-tempo))
+  (org-refile-allow-creating-parent-nodes 'confirm)
+  (org-refile-targets '((org-agenda-files :maxlevel . 1)
+                        ("~/.personal/agenda/home.org" :maxlevel . 2)
+                        ("~/.personal/agenda/work.org" :maxlevel . 2)))
+  (org-refile-use-cache nil)
+  (org-refile-use-outline-path nil)
+  (org-startup-indented t)
+  (org-startup-with-inline-images t)
+  (org-tag-alist
+   '((:startgroup . "Context")
+     ("@errands" . ?e)
+     ("@home" . ?h)
+     ("@work" . ?w)
+     (:endgroup)
+     (:startgroup . "Difficulty")
+     ("easy" . ?E)
+     ("medium" . ?M)
+     ("challenging" . ?C)
+     (:endgroup)
+     ("bug" . ?b)
+     ("car" . ?v)
+     ("future" . ?F)
+     ("goal" . ?g)
+     ("health" . ?H)
+     ("house" . ?O)
+     ("meeting" . ?m)
+     ("planning" . ?p)
+     ("phone" . ?0)
+     ("purchase" . ?P)
+     ("reading" . ?r)
+     ("review" . ?R)
+     ("study" . ?s)
+     ("sport" . ?S)
+     ("talk" . ?T)
+     ("tech" . ?t)
+     ("trip" . ?I)
+     ("thinking" . ?i)
+     ("update" . ?u)
+     ("watch" . ?l)
+     ("writing" . ?W)))
+  (org-tags-exclude-from-inheritance '("crypt" "project"))
+  (org-todo-keywords '((sequence "TODO(t)"
+                                 "STARTED(s)"
+                                 "NEXT(n)"
+                                 "SOMEDAY(.)"
+                                 "WAITING(w)""|" "DONE(x!)" "CANCELLED(c@)")))
+  (org-use-effective-time t)
+  (org-use-speed-commands 'my/org-use-speed-commands-for-headings-and-lists)
+  (org-yank-adjusted-subtrees t)
+  :config
+  (add-to-list 'org-global-properties '("Effort_ALL". "0:05 0:15 0:30 1:00 2:00 3:00 4:00"))
+  (add-to-list 'org-speed-commands '("$" call-interactively 'org-archive-subtree))
+  (add-to-list 'org-speed-commands '("i" call-interactively 'org-clock-in))
+  (add-to-list 'org-speed-commands '("o" call-interactively 'org-clock-out))
+  (add-to-list 'org-speed-commands '("s" call-interactively 'org-schedule))
+  (add-to-list 'org-speed-commands '("x" org-todo "DONE"))
+  (add-to-list 'org-speed-commands '("y" org-todo-yesterday "DONE"))
+  (add-to-list 'org-structure-template-alist '("el" . "src emacs-lisp"))
+  (add-to-list 'org-structure-template-alist '("sh" . "src shell"))
+  (add-to-list 'org-structure-template-alist '("py" . "src python"))
+  (advice-add 'org-deadline :after (ignore-args #'org-save-all-org-buffers))
+  (advice-add 'org-schedule :after (ignore-args #'org-save-all-org-buffers))
+  (advice-add 'org-store-log-note :after (ignore-args #'org-save-all-org-buffers))
+  (advice-add 'org-refile :after 'org-save-all-org-buffers)
+  (advice-add 'org-todo :after (ignore-args #'org-save-all-org-buffers))
+  ;; Replace list hyphen with dot
+  (font-lock-add-keywords 'org-mode
+                          '(("^ *\\([-]\\) "
+                             (0 (prog1 () (compose-region (match-beginning 1) (match-end 1) "•"))))))
+  (org-clock-persistence-insinuate)
+  (org-load-modules-maybe t))
+
+
+  ;;; MORE AGENDA STUFF
+
+  (use-package org-agenda
+  :ensure nil
+  :bind (:map org-agenda-mode-map
+              ("C-n" . org-agenda-next-item)
+              ("C-p" . org-agenda-previous-item)
+              ("j" . org-agenda-goto)
+              ("X" . my/org-agenda-mark-done-next)
+              ("x" . my/org-agenda-mark-done))
+  :preface
+  (defun my/org-agenda-mark-done (&optional arg)
+    "Mark the current TODO as done in org-agenda."
+    (interactive "P")
+    (org-agenda-todo "DONE"))
+
+  (defun my/org-agenda-mark-done-next ()
+    "Mark the current TODO as done and add another task after it."
+    (interactive)
+    (org-agenda-todo "DONE")
+    (org-agenda-switch-to)
+    (org-capture 0 "t"))
+  :custom
+  (org-agenda-category-icon-alist
+   `(("home" ,(list (all-the-icons-faicon "home" :v-adjust -0.05)) nil nil :ascent center :mask heuristic)
+     ("inbox" ,(list (all-the-icons-faicon "inbox" :v-adjust -0.1)) nil nil :ascent center :mask heuristic)
+     ("people" ,(list (all-the-icons-material "people" :v-adjust -0.25)) nil nil :ascent center :mask heuristic)
+     ("work" ,(list (all-the-icons-material "work" :v-adjust -0.25)) nil nil :ascent center :mask heuristic)
+     ("routine" ,(list (all-the-icons-material "repeat" :v-adjust -0.25)) nil nil :ascent center :mask heuristic)
+     ))
+  (org-agenda-custom-commands
+   '(("d" "Dashboard"
+      ((agenda "" ((org-deadline-warning-days 7)))
+       (todo "NEXT"
+             ((org-agenda-overriding-header "Next Tasks")))
+       (tags-todo "agenda/ACTIVE" ((org-agenda-overriding-header "Active Projects")))))
+
+     ("n" "Next Tasks"
+      ((agenda "" ((org-deadline-warning-days 7)))
+       (todo "NEXT"
+             ((org-agenda-overriding-header "Next Tasks")))))
+
+     ("h" "Home Tasks" tags-todo "@home")
+     ("w" "Work Tasks" tags-todo "@work")
+
+     ("E" "Easy Tasks" tags-todo "easy")
+     ("C" "Challenging Tasks" tags-todo "challenging")
+
+     ("e" tags-todo "+TODO=\"NEXT\"+Effort<15&+Effort>0"
+      ((org-agenda-overriding-header "Low Effort Tasks")
+       (org-agenda-max-todos 20)
+       (org-agenda-files org-agenda-files)))))
+  (org-agenda-dim-blocked-tasks t)
+ ;; (org-agenda-files '("~/.personal/agenda"))
+  (org-agenda-inhibit-startup t)
+  (org-agenda-show-log t)
+  (org-agenda-skip-deadline-if-done t)
+  (org-agenda-skip-deadline-prewarning-if-scheduled 'pre-scheduled)
+  (org-agenda-skip-scheduled-if-done t)
+  (org-agenda-span 2)
+  (org-agenda-start-on-weekday 6)
+  (org-agenda-start-with-log-mode t)
+  (org-agenda-sticky nil)
+  (org-agenda-tags-column 90)
+  (org-agenda-time-grid '((daily today require-timed)))
+  (org-agenda-use-tag-inheritance t)
+  (org-columns-default-format "%14SCHEDULED %Effort{:} %1PRIORITY %TODO %50ITEM %TAGS")
+  (org-default-notes-file "~/.personal/agenda/inbox.org")
+ ;; (org-directory "~/.personal")
+  (org-enforce-todo-dependencies t)
+  (org-habit-completed-glyph ?✓)
+  (org-habit-graph-column 80)
+  (org-habit-show-habits-only-for-today nil)
+  (org-habit-today-glyph ?‖)
+  (org-track-ordered-property-with-tag t))
+
+
+  ;;; NOTIFIER
+
+  (use-package org-wild-notifier
+  :after org
+  :custom
+  (alert-default-style 'libnotify)
+  (org-wild-notifier-notification-title "Agenda Reminder")
+  :config (org-wild-notifier-mode))
+
+
+  ;;; ORG CAPTURE
+
+  (use-package org-capture
+  :ensure nil
+  :preface
+  (defvar my/org-active-task-template
+    (concat "* NEXT %^{Task}\n"
+            ":PROPERTIES:\n"
+            ":Effort: %^{effort|1:00|0:05|0:15|0:30|2:00|4:00}\n"
+            ":CAPTURED: %<%Y-%m-%d %H:%M>\n"
+            ":END:") "Template for basic task.")
+  (defvar my/org-appointment
+    (concat "* TODO %^{Appointment}\n"
+            "SCHEDULED: %t\n") "Template for appointment task.")
+  (defvar my/org-basic-task-template
+    (concat "* TODO %^{Task}\n"
+            ":PROPERTIES:\n"
+            ":Effort: %^{effort|1:00|0:05|0:15|0:30|2:00|4:00}\n"
+            ":CAPTURED: %<%Y-%m-%d %H:%M>\n"
+            ":END:") "Template for basic task.")
+  (defvar my/org-contacts-template
+    (concat "* %(org-contacts-template-name)\n"
+            ":PROPERTIES:\n"
+            ":BIRTHDAY: %^{YYYY-MM-DD}\n"
+            ":END:") "Template for a contact.")
+  :custom
+  (org-capture-templates
+   `(
+     ("c" "Contact" entry (file+headline "~/.personal/agenda/contacts.org" "Inbox"),
+      my/org-contacts-template
+      :empty-lines 1)
+
+     ("p" "People" entry (file+headline "~/.personal/agenda/people.org" "Tasks"),
+      my/org-basic-task-template
+      :empty-lines 1)
+     ("a" "Appointment" entry (file+headline "~/.personal/agenda/people.org" "Appointments"),
+      my/org-appointment
+      :empty-lines 1)
+     ("m" "Meeting" entry (file+headline "~/.personal/agenda/people.org" "Meetings")
+      "* Meeting with %? :meeting:\n%U" :clock-in t :clock-resume t :empty-lines 1)
+     ("P" "Phone Call" entry (file+headline "~/.personal/agenda/people.org" "Phone Calls")
+      "* Phone %? :phone:\n%U" :clock-in t :clock-resume t)
+
+     ("i" "New Item")
+     ("ib" "Book" checkitem (file+headline "~/.personal/items/books.org" "Books")
+      "- [ ] %^{Title} - %^{Author}\n  %U"
+      :immediate-finish t)
+     ("il" "Learning" checkitem (file+headline "~/.personal/items/learning.org" "Things")
+      "- [ ] %^{Thing}\n  %U"
+      :immediate-finish t)
+     ("im" "Movie" checkitem (file+headline "~/.personal/items/movies.org" "Movies")
+      "- [ ] %^{Title}\n  %U"
+      :immediate-finish t)
+     ("ip" "Purchase" checkitem (file+headline "~/.personal/items/purchases.org" "Purchases")
+      "- [ ] %^{Item}\n  %U"
+      :immediate-finish t)
+
+     ("t" "New Task")
+     ("ta" "Active" entry (file+headline "~/.personal/agenda/inbox.org" "Active"),
+      my/org-active-task-template
+      :empty-lines 1
+      :immediate-finish t)
+     ("tb" "Backlog" entry (file+headline "~/.personal/agenda/inbox.org" "Backlog"),
+      my/org-basic-task-template
+      :empty-lines 1
+      :immediate-finish t))))
+
+;; ORG CLOCK
+(use-package org-clock
+  :ensure nil
+  :after org
+  :preface
+  (defun my/org-mode-ask-effort ()
+    "Ask for an effort estimate when clocking in."
+    (unless (org-entry-get (point) "Effort")
+      (let ((effort
+             (completing-read
+              "Effort: "
+              (org-entry-get-multivalued-property (point) "Effort"))))
+        (unless (equal effort "")
+          (org-set-property "Effort" effort)))))
+  :hook (org-clock-in-prepare-hook . my/org-mode-ask-effort)
+  :custom
+  (org-clock-clocktable-default-properties
+   '(:block thisweek :maxlevel 2 :scope agenda :link t :compact t :formula %
+            :step week :fileskip0 t :stepskip0 t :narrow 50
+            :properties ("Effort" "CLOCKSUM" "TODO")))
+  (org-clock-continuously nil)
+  (org-clock-in-switch-to-state "STARTED")
+  (org-clock-out-remove-zero-time-clocks t)
+  (org-clock-persist t)
+  (org-clock-persist-file (expand-file-name (format "%s/emacs/org-clock-save.el" xdg-cache)))
+  (org-clock-persist-query-resume nil)
+  (org-clock-report-include-clocking-task t)
+  (org-show-notification-handler (lambda (msg) (alert msg))))
+
+;; PRG LANGUAGES
+
+(use-package python :ensure nil :after org)
+(use-package ob-C :ensure nil :after org)
+(use-package ob-css :ensure nil :after org)
+(use-package ob-dot :ensure nil :after org)
+(use-package ob-emacs-lisp :ensure nil :after org)
+(use-package ob-gnuplot :ensure nil :after org)
+(use-package ob-java :ensure nil :after org)
+(use-package ob-js :ensure nil :after org)
+(use-package ob-latex
+  :ensure nil
+  :after org
+  :custom (org-latex-compiler "xelatex"))
+(use-package ob-makefile :ensure nil :after org)
+(use-package ob-org :ensure nil :after org)
+
+(use-package ob-python :ensure nil :after org)
+(use-package ob-shell :ensure nil :after org)
+(use-package ob-sql :ensure nil :after org)
+
+
+;; ORG NOTES AND ROAM
+(use-package org-roam
+  :after org
+  :init
+  (setq org-roam-v2-ack t)
+  (setq my/daily-note-filename "%<%Y-%m-%d>.org.gpg"
+        my/daily-note-header "#+title: %<%Y-%m-%d %a>\n\n[[roam:%<%Y-%B>]]\n\n")
+  :custom
+  (org-roam-capture-templates
+   '(("d" "default" plain "%?"
+      :if-new (file+head "%<%Y%m%d%H%M%S>-${slug}.org"
+                         "#+title: ${title}\n")
+      :unnarrowed t)))
+  (org-roam-completion-everywhere t)
+  (org-roam-dailies-directory "journal/")
+  (org-roam-dailies-capture-templates
+   `(("d" "default" plain
+      "* %?"
+      :if-new (file+head ,my/daily-note-filename
+                         ,my/daily-note-header)
+      :empty-lines 1)
+
+     ("j" "journal" plain
+      "** %<%I:%M %p>  :journal:\n\n%?\n\n"
+      :if-new (file+head+olp ,my/daily-note-filename
+                             ,my/daily-note-header
+                             ("Journal"))
+      :empty-lines 1)
+     ("m" "meeting" entry
+      "** %<%I:%M %p> - %^{Meeting Title}  :meeting:\n\n%?\n\n"
+      :if-new (file+head+olp ,my/daily-note-filename
+                             ,my/daily-note-header
+                             ("Meetings"))
+      :empty-lines 1)))
+  ;; (org-roam-directory "~/.personal/notes")
+  :custom (org-roam-graph-viewer "/usr/bin/firefox")
+  :config (org-roam-setup))
 
 :;;;;;;;;;;;;;;;;;;;;;;;; ORG MODE STUFF ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 
