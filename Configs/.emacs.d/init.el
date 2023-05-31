ca380ab867bf7daa376e7d0e521dc0c124d427dc
Akash <ag1884@york.ac.uk>
2023-05-31 15:41:57 +0100

    Updated init.el

--- Configs/.emacs.d/init.el
+++ Configs/.emacs.d/init.el
@@ -1124,7 +1124,24 @@
   (setq-default flycheck-disabled-checkers '(c/c++-clang c/c++-cppcheck c/c++-gcc))
   :hook ((c-mode c++-mode objc-mode) .
          (lambda () (require 'ccls) (lsp))))
-  
+
+
+(which-key-mode)
+(add-hook 'c-mode-hook 'lsp)
+(add-hook 'c++-mode-hook 'lsp)
+
+(setq gc-cons-threshold (* 100 1024 1024)
+      read-process-output-max (* 1024 1024)
+      treemacs-space-between-root-nodes nil
+      company-idle-delay 0.0
+      company-minimum-prefix-length 1
+      lsp-idle-delay 0.1)  ;; clangd is fast
+
+(with-eval-after-load 'lsp-mode
+  (add-hook 'lsp-mode-hook #'lsp-enable-which-key-integration)
+  (require 'dap-cpptools)
+  (yas-global-mode))
+
 
   (use-package platformio-mode)
   ;; edit ino files with adruino mode. 
