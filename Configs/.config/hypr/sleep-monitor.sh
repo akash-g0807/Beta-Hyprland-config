eac0f850be7a4b464392d12bdc96edf97194bb6a
Akash Gopinath <ag1884@york.ac.uk>
2023-09-11 09:15:22 +0100

    Updated sleep monitor script

--- Configs/.config/hypr/sleep-monitor.sh
+++ Configs/.config/hypr/sleep-monitor.sh
@@ -1,5 +1,5 @@
 if grep open /proc/acpi/button/lid/LID*/state; then
-    hyprctl keyword monitor "eDP-1, 1920x1080, 0x0, 1"
+    hyprctl keyword monitor "eDP-1, 3480x2400, 0x0, 2"
 else
     if [[ `hyprctl monitors | grep "Monitor" | wc -l` != 1 ]]; then
         hyprctl keyword monitor "eDP-1, disable"
