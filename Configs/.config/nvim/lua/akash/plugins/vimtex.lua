435bbcc2ddf84a7cdcfc966c3ab5ed16e6b89b86
Akash Gopinath <ag1884@york.ac.uk>
2023-09-16 22:11:29 +0100

    Neovim config vimtex updated

--- Configs/.config/nvim/lua/akash/plugins/vimtex.lua
+++ Configs/.config/nvim/lua/akash/plugins/vimtex.lua
@@ -16,16 +16,19 @@ vim.g["vimtex_syntax_enabled"] = 1
 -- https://github.com/lervag/vimtex/blob/master/doc/vimtex.txt
 
 vim.g["vimtex_log_ignore"] = {
-  "Underfull",
-  "Overfull",
-  "specifier changed to",
-  "Token not allowed in a PDF string",
+	"Underfull",
+	"Overfull",
+	"specifier changed to",
+	"Token not allowed in a PDF string",
 }
 
 vim.g["vimtex_context_pdf_viewer"] = "okular"
-
+vim.opt.conceallevel = 1
 -- vim.g["vimtex_compiler_method"] = "latexrun"
 
 -- vim.g['vimtex_complete_enabled'] = 1
 -- vim.g['vimtex_compiler_progname'] = 'nvr'
--- vim.g['vimtex_complete_close_braces'] = 1
+-- vim.g['vimtex_complete_close_braces'] i= 1
+--
+vim.g["vimtex_quickfix_mode"] = 0
+vim.g["tex_conceal"] = "abdmg"
