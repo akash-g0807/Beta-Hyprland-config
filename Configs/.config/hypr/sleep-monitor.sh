6374aed3f0565c5449dcd28d779d1554efc4763f
Akash Gopinath <akash.gopinath008@gmail.com>
2024-12-16 14:10:32 +0000

    Added some plugins and qt apps scaling

--- Configs/.config/hypr/sleep-monitor.sh
+++ Configs/.config/hypr/sleep-monitor.sh
@@ -1,7 +1,7 @@
 if grep open /proc/acpi/button/lid/LID0/state; then
-    hyprctl keyword monitor ", highres, auto, 1.56666666666"
+    hyprctl keyword monitor "eDP-1, 2256x1504, auto, 1.56666666666"
 else
     if [[ `hyprctl monitors | grep "Monitor" | wc -l` != 1 ]]; then
         hyprctl keyword monitor "eDP-1, disable"
-    fi
+    fi 
 fi
--- Configs/.scripts/change_theme_dark.sh
+++ Configs/.scripts/change_theme_dark.sh
@@ -15,4 +15,4 @@ flatpak --user override --env=GTK_THEME=Catppuccin-Mocha-Standard-Blue-Dark
 flatpak --user override --env=ICON_THEME=Papirus-Dark
 flatpak --user override --env=XCURSOR_PATH=/home/$USER/.icons
 flatpak --user override --env=XCURSOR_THEME=Nordzy-cursors
-flatpal --user override --env=XCURSOR_THEME=28
+flatpal --user override --env=XCURSOR_THEME=24
